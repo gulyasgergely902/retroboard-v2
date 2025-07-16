6ef7edad9046808835510598876b70c33ff86a05
Gulyas Gergely <gulyas.gergely@hotmail.com>
2025-07-16 15:04:03 +0200

    Fix colors, format code, add create board button

--- frontend/src/components/BoardCard.vue
+++ frontend/src/components/BoardCard.vue
@@ -1,12 +1,12 @@
 <template>
   <div
-    class="bg-sky-500 text-sky-950 hover:bg-sky-700 dark:bg-sky-950 dark:text-sky-50 transition-colors p-4 rounded-xl h-32"
+    class="bg-sky-500 dark:bg-sky-900 text-sky-950 dark:text-sky-50 hover:bg-sky-600 dark:hover:bg-sky-950 transition-colors p-4 rounded-xl h-32"
   >
     {{ title }}
   </div>
 </template>
 
 <script setup lang="ts">
-import { defineProps } from 'vue'
-defineProps<{ title: string }>()
+  import { defineProps } from 'vue'
+  defineProps<{ title: string }>()
 </script>
