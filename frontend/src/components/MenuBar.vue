593de62b22467bb5ca5a4d81e700e68466bee7cd
Gulyas Gergely <gulyas.gergely@hotmail.com>
2025-07-21 17:15:50 +0200

    Apply colors on home page

--- frontend/src/components/MenuBar.vue
+++ frontend/src/components/MenuBar.vue
@@ -1,11 +1,11 @@
 <template>
-  <nav class="bg-white border-gray-200 dark:bg-slate-900">
+  <nav>
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <RouterLink
         to="/"
         class="flex items-center space-x-3 rtl:space-x-reverse"
       >
-        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-slate-50">
+        <span class="text-color self-center text-2xl font-semibold whitespace-nowrap">
           RetroBoard
         </span>
       </RouterLink>
