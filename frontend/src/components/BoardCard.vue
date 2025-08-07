7bdafccafb8721d1f1c3bf4da4e6ab4b3c7c026c
Gulyas Gergely <gulyas.gergely@hotmail.com>
2025-08-07 21:08:00 +0200

    frontend: Lint frontend

--- frontend/src/components/BoardCard.vue
+++ frontend/src/components/BoardCard.vue
@@ -3,11 +3,15 @@
     class="background-color text-color transition-colors p-4 rounded-md border-1 border-color h-32"
   >
     <p class="text-color text-lg">{{ title }}</p>
-    <p class="background-color-primary text-color-over-primary text-xs inline-block rounded-full py-1 px-2 mt-2">Notes: {{ noteCount }}</p>
+    <p
+      class="background-color-primary text-color-over-primary text-xs inline-block rounded-full py-1 px-2 mt-2"
+    >
+      Notes: {{ noteCount }}
+    </p>
   </div>
 </template>
 
 <script setup lang="ts">
   import { defineProps } from 'vue'
-  defineProps<{ title: string, noteCount: number }>()
+  defineProps<{ title: string; noteCount: number }>()
 </script>
