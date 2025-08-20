2022d82f1381ad6915167f1532736296e328fd8f
Gulyas Gergely <gulyas.gergely@hotmail.com>
2025-08-20 14:54:23 +0200

    backend: Add api response dataclass

---
+++ backend/custom_types/api_response.py
@@ -0,0 +1,11 @@
+"""ApiResponse"""
+
+from dataclasses import dataclass
+from typing import Any, Dict, List
+
+
+@dataclass
+class ApiResponse:
+    """API Response Dataclass for Service Responses"""
+    response: List | Dict[str, Any]
+    status_code: int
